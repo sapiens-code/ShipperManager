f2da983ed1ec3b3be6f078869e3cd99c4dd90c46
HoangCuongYuuki <58560483+HoangCuongYuuki@users.noreply.github.com>
2022-05-28 16:18:25 +0700

    edit UI

--- ShipperManagerWeb/ShipperManager/Models/PhuongThucThanhToan.cs
+++ ShipperManagerWeb/ShipperManager/Models/PhuongThucThanhToan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace ShipperManager.Models
     public class PhuongThucThanhToan
     {
         public string Id { get; set; }
+        [Display(Name ="Tên")]
         public string Ten { get; set; }
+        [Display(Name = "Mô tả")]
         public string MoTa { get; set; }
         public PhuongThucThanhToan()
         {
